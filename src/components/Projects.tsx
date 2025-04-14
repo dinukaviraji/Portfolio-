1736cd576bac1f82ca8b15bdfd3c5ccd9c4da770
Dinuka Viraji <dinukaviraji123@gmail.com>
2025-04-14 07:30:08 +0530

    add anothe project

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,4 +1,3 @@
-// import finance from '../assets/financeTracker.jpg';
 import finance1 from '../assets/finance2.png';
 
 const Projects: React.FC = () => {
@@ -15,8 +14,8 @@ const Projects: React.FC = () => {
         //         </div>
         //     </div>
         // </div>
-
-        <div className="flex flex-wrap justify-center bg-gray-100 w-200 h-100 mx-auto p-4 mb-300">
+        <>
+        <div className="flex flex-wrap justify-center bg-gray-100 w-200 h-100 mx-auto p-4 mb-20">
 
         <div className="flex w-full max-w-4xl bg-white rounded-xl  overflow-hidden">
 
@@ -36,7 +35,35 @@ const Projects: React.FC = () => {
             <img src={finance1}  alt="Letter Loco" className="w-full h-full object-cover" />
           </div>
         </div>
-      </div>
+        </div>
+
+
+        <div className="flex flex-wrap justify-center bg-gray-100 w-200 h-100 mx-auto p-4 mb-300">
+
+        <div className="flex w-full max-w-4xl bg-white rounded-xl  overflow-hidden">
+
+          <div className="w-full md:w-1/2 p-6 flex flex-col justify-center">
+            <h2 className="text-2xl font-bold text-gray-800 mb-2">Student Management System</h2>
+            <p> Java </p>
+            <p className="text-gray-600">A Java-based console application designed to streamline student registration and academic performance tracking for universities. It supports student data management, module mark entry, automatic grade calculation, and the generation of both summary and detailed performance reports. Key features include file handling, custom sorting, and object-oriented design principles.
+
+            </p>
+            {/* <button className="mt-auto self-start bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700">
+              Learn More
+            </button> */}
+          </div>
+  
+
+          <div className="w-full md:w-1/2">
+            <img src={finance1}  alt="Letter Loco" className="w-full h-full object-cover" />
+          </div>
+        </div>
+        </div>
+        </>          
+
+
+
+              
         
     );
 }
