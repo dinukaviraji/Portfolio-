b09353ee265fd627327a2d78687d1f894552b1db
Dinuka Viraji <dinukaviraji123@gmail.com>
2025-04-16 15:11:04 +0530

    modified

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,4 +1,7 @@
-import finance1 from '../assets/finance2.png';
+import finance1 from '../assets/finance.png';
+import sdgp from '../assets/sdgp.png';
+import student from '../assets/studentManagement.png';
+import youtube from '../assets/youtube.svg';
 
 const Projects: React.FC = () => {
     return (
@@ -29,16 +32,16 @@ const Projects: React.FC = () => {
               Learn More
             </button> */}
           </div>
-  
 
-          <div className="w-full md:w-1/2">
-            <img src={finance1}  alt="Letter Loco" className="w-full h-full object-cover" />
+          <div className="w-full md:w-1/2 flex items-center justify-center">
+            <img src={finance1}  alt="GUI-financeTracker" className="w-full h-auto object-contain shadow-2xl rounded-lg ring-1 ring-gray-200 hover:scale-110 transition-transform duration-400 " />
           </div>
         </div>
         </div>
 
 
-        <div className="flex flex-wrap justify-center bg-gray-100 w-200 h-100 mx-auto p-4 mb-300">
+
+        <div className="flex flex-wrap justify-center bg-gray-100 w-200 h-100 mx-auto p-4 mb-20">
 
         <div className="flex w-full max-w-4xl bg-white rounded-xl  overflow-hidden">
 
@@ -52,10 +55,34 @@ const Projects: React.FC = () => {
               Learn More
             </button> */}
           </div>
-  
 
-          <div className="w-full md:w-1/2">
-            <img src={finance1}  alt="Letter Loco" className="w-full h-full object-cover" />
+          <div className="w-full md:w-1/2 flex items-center justify-center">
+            <img src={student}  alt="Letter Loco" className="w-full h-auto object-contain rounded-lg shadow-2xl ring-1 ring-gray-200 hover:scale-110 transition-transform duration-400"
+            />
+          </div>
+        </div>
+        </div>
+
+
+        <div className="flex flex-wrap justify-center bg-gray-100 w-200 h-100 mx-auto p-4 mb-300">
+
+        <div className="flex w-full max-w-4xl bg-white rounded-xl  overflow-hidden">
+
+          <div className="w-full md:w-1/2 p-6 flex flex-col justify-center">
+            <h2 className="text-2xl font-bold text-gray-800 mb-2">Vyakarana  </h2>
+            <p> TypeScript | Python | HTML | CSS </p>
+            <p className="text-gray-600 mt-3">A Sinhala grammar checker and language learning tool that helps users write accurately and confidently. Vyakarana detects grammar errors and suggests corrections to improve writing. It also supports learning Sinhala by offering Sinhala grammar rules, synonyms, antonyms and paired words.
+
+            </p>
+            <button className="mt-auto self-start bg-amber-600 text-white px-4 py-2 rounded-4xl flex hover:scale-105">
+              <img src={youtube} /> 
+              Demo
+            </button>
+          </div>
+
+
+          <div className="w-full  md:w-1/2 flex items-center justify-center">
+            <img src={sdgp}  alt="Vyakarana" className="w-full h-auto object-contain shadow-2xl rounded-lg ring-1 ring-gray-200 hover:scale-110 transition-transform duration-400 "/>
           </div>
         </div>
         </div>
