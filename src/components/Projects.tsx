450b5e2d3f30d2cc529538df5159af95c83cec14
Dinuka Viraji <dinukaviraji123@gmail.com>
2025-04-21 00:38:12 +0530

    modified

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -6,6 +6,7 @@ import marineLife from '../assets/marineLife.png';
 import selfOrder from '../assets/images.jpg';
 import ticketingSystem from '../assets/ticket.jpg';
 import portfolio from '../assets/portfolio.png';
+import github from '../assets/github-white.svg';
 
 const Projects: React.FC = () => {
     return (
@@ -14,7 +15,7 @@ const Projects: React.FC = () => {
         <p className='flex justify-center mb-7 text-2xl font-semibold underline'> My Projects </p>
 
 
-        <div className="flex flex-wrap justify-center bg-gray-100 w-200 h-100 mx-auto p-4 mb-20 shadow-md hover:scale-105 transition-transform duration-300">
+        <div className="flex flex-wrap justify-center bg-gray-100 w-200 h-100 mx-auto p-4 mb-20 shadow-md ">
 
         <div className="flex w-full max-w-4xl bg-white rounded-xl overflow-hidden">
 
@@ -24,11 +25,18 @@ const Projects: React.FC = () => {
             <p className="text-gray-600 mt-3">A Sinhala grammar checker and language learning tool that helps users write accurately and confidently. Vyakarana detects grammar errors and suggests corrections to improve writing. It also supports learning Sinhala by offering Sinhala grammar rules, synonyms, antonyms and paired words.
 
             </p>
-            <button className="mt-auto self-start bg-amber-600 text-white px-4 py-2 rounded-4xl flex hover:scale-105 cursor-pointer gap-1" 
+            <div className='flex mt-5 gap-5 '>
+            <button className=" bg-amber-600 text-white px-4 py-2 rounded-4xl flex hover:scale-105 duration-150 cursor-pointer gap-1" 
               onClick={() => window.open('https://youtu.be/drXpTm61dBU', '_blank')}>
               <img src={youtube} /> 
               Demo
             </button>
+            <button className='bg-gray-700 text-white px-4 py-2 rounded-4xl flex hover:scale-105 duration-150 cursor-pointer gap-1' 
+            onClick={() => window.open('https://github.com/dinukaviraji/Sinhala-Grammar-Checking-and-Learning-Tool-SDGP-', '_blank')}>
+              <img src={github}/>
+              GitHub
+            </button>
+            </div>
           </div>
 
           <div className="w-full  md:w-1/2 flex items-center justify-center">
@@ -39,7 +47,7 @@ const Projects: React.FC = () => {
 
 
 
-        <div className="flex flex-wrap justify-center bg-gray-100 w-200 h-100 mx-auto p-4 mb-20 shadow-md hover:scale-105 transition-transform duration-300">
+        <div className="flex flex-wrap justify-center bg-gray-100 w-200 h-100 mx-auto p-4 mb-20 shadow-md ">
 
         <div className="flex w-full max-w-4xl bg-white rounded-xl  overflow-hidden">
 
@@ -47,13 +55,20 @@ const Projects: React.FC = () => {
             <h2 className="text-2xl font-bold text-gray-800 mb-2">Life Below Water </h2>
             <p> HTML | CSS | JavaScript</p>
             <p className="text-gray-600 mt-3">A website dedicated to marine life, inspired by the UN’s Sustainable Development Goals. It features a splash screen, homepage, gallery, shop, user profiles, feedback form, sitemap, and team pages raising awareness and promoting ocean conservation through an interactive user experience.
-
             </p>
+
+            <div className='flex mt-5 gap-5 '>
             <button className="mt-auto self-start bg-amber-600 text-white px-4 py-2 rounded-4xl flex hover:scale-105 cursor-pointer gap-1" 
             onClick={() => window.open('https://isuruk2003.github.io/web-dev-group-coursework-iit/student_1/splash.html', '_blank')}>
               <img src={youtube} /> 
               Demo
             </button>
+            <button className='bg-gray-700 text-white px-4 py-2 rounded-4xl flex hover:scale-105 duration-150 cursor-pointer gap-1' 
+            onClick={() => window.open('https://github.com/dinukaviraji/Life-Below-Water', '_blank')}>
+              <img src={github}/>
+              GitHub
+            </button>
+            </div>
           </div>
           <div className="w-full  md:w-1/2 flex items-center justify-center">
             <img src={marineLife}  alt="LifeBelowWater" className="w-full h-auto object-contain shadow-2xl rounded-lg ring-1 ring-gray-200 hover:scale-110 transition-transform duration-400 " />
@@ -62,7 +77,8 @@ const Projects: React.FC = () => {
         </div>
 
 
-        <div className="flex flex-wrap justify-center bg-gray-100 w-200 h-100 mx-auto p-4 mb-20 shadow-md hover:scale-105 transition-transform duration-300">
+
+        <div className="flex flex-wrap justify-center bg-gray-100 w-200 h-100 mx-auto p-4 mb-20 shadow-md ">
 
         <div className="flex w-full max-w-4xl bg-white rounded-xl  overflow-hidden">
 
@@ -70,13 +86,16 @@ const Projects: React.FC = () => {
             <h2 className="text-2xl font-bold text-gray-800 mb-2">Self Ordering System </h2>
             <p> C# | SQL </p>
             <p className="text-gray-600 mt-3">A desktop application developed using C# and SQL for restaurants to streamline the ordering process. Customers can browse the menu, place orders directly, and view their bill, making the dining experience faster and more efficient while reducing staff workload.
-
             </p>
-            {/* <button className="mt-auto self-start bg-amber-600 text-white px-4 py-2 rounded-4xl flex hover:scale-105 cursor-pointer gap-1" 
-            onClick={() => window.open('https://isuruk2003.github.io/web-dev-group-coursework-iit/student_1/splash.html', '_blank')}>
-              <img src={youtube} /> 
-              Demo
-            </button> */}
+
+            <div className='flex mt-5 gap-5 '>
+            <button className='bg-gray-700 text-white px-4 py-2 rounded-4xl flex hover:scale-105 duration-150 cursor-pointer gap-1' 
+            onClick={() => window.open('https://github.com/dinukaviraji/Self-Ordering-System', '_blank')}>
+              <img src={github}/>
+              GitHub
+            </button>
+            </div>
+
           </div>
           <div className="w-full  md:w-1/2 flex items-center justify-center">
             <img src={selfOrder}  alt="SelfOrderingMachine" className="w-full h-auto object-contain shadow-2xl rounded-lg ring-1 ring-gray-200 hover:scale-110 transition-transform duration-400 " />
@@ -86,7 +105,7 @@ const Projects: React.FC = () => {
 
 
 
-        <div className="flex flex-wrap justify-center bg-gray-100 w-200 h-100 mx-auto p-4 mb-20 shadow-md hover:scale-105 transition-transform duration-300">
+        <div className="flex flex-wrap justify-center bg-gray-100 w-200 h-100 mx-auto p-4 mb-20 shadow-md ">
 
         <div className="flex w-full max-w-4xl bg-white rounded-xl  overflow-hidden">
 
@@ -99,6 +118,13 @@ const Projects: React.FC = () => {
             {/* <button className="mt-auto self-start bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700">
               Learn More
             </button> */}
+            <div className='flex mt-5 gap-5 '>
+            <button className='bg-gray-700 text-white px-4 py-2 rounded-4xl flex hover:scale-105 duration-150 cursor-pointer gap-1' 
+            onClick={() => window.open('https://github.com/dinukaviraji/Personal-Finance-Tracker', '_blank')}>
+              <img src={github}/>
+              GitHub
+            </button>
+            </div>
           </div>
 
           <div className="w-full md:w-1/2 flex items-center justify-center">
@@ -109,19 +135,26 @@ const Projects: React.FC = () => {
 
 
 
-        <div className="flex flex-wrap justify-center bg-gray-100 w-200 h-100 mx-auto p-4 mb-20 shadow-md hover:scale-105 transition-transform duration-300">
+        <div className="flex flex-wrap justify-center bg-gray-100 w-200 h-100 mx-auto p-4 mb-20 shadow-md ">
 
         <div className="flex w-full max-w-4xl bg-white rounded-xl  overflow-hidden">
 
           <div className="w-full md:w-1/2 p-6 flex flex-col justify-center">
             <h2 className="text-2xl font-bold text-gray-800 mb-2">Student Management System</h2>
             <p> Java </p>
-            <p className="text-gray-600 mt-3">A Java-based console application designed to streamline student registration and academic performance tracking for universities. It supports student data management, module mark entry, automatic grade calculation, and the generation of both summary and detailed performance reports. Key features include file handling, custom sorting, and object-oriented design principles.
+            <p className="text-gray-600 mt-3">A Java-based console application for managing student registration and academic performance in universities. It supports data management, module mark entry, grade calculation, and generates detailed reports, using file handling, custom sorting, and object-oriented design.
 
             </p>
             {/* <button className="mt-auto self-start bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700">
               Learn More
             </button> */}
+            <div className='flex mt-5 gap-5 '>
+            <button className='bg-gray-700 text-white px-4 py-2 rounded-4xl flex hover:scale-105 duration-150 cursor-pointer gap-1' 
+            onClick={() => window.open('https://github.com/dinukaviraji/Student-Management-System', '_blank')}>
+              <img src={github}/>
+              GitHub
+            </button>
+            </div>
           </div>
 
           <div className="w-full md:w-1/2 flex items-center justify-center">
@@ -134,7 +167,7 @@ const Projects: React.FC = () => {
 
 
 
-        <div className="flex flex-wrap justify-center bg-gray-100 w-200 h-100 mx-auto p-4 mb-20 shadow-md hover:scale-105 transition-transform duration-300">
+        <div className="flex flex-wrap justify-center bg-gray-100 w-200 h-100 mx-auto p-4 mb-20 shadow-md">
 
         <div className="flex w-full max-w-4xl bg-white rounded-xl  overflow-hidden">
 
@@ -149,6 +182,13 @@ const Projects: React.FC = () => {
               <img src={youtube} /> 
               Demo
             </button> */}
+             <div className='flex mt-5 gap-5 '>
+            <button className='bg-gray-700 text-white px-4 py-2 rounded-4xl flex hover:scale-105 duration-150 cursor-pointer gap-1' 
+            onClick={() => window.open('https://github.com/dinukaviraji/Real-Time-Ticketing-System', '_blank')}>
+              <img src={github}/>
+              GitHub
+            </button>
+            </div>
           </div>
 
           <div className="w-full  md:w-1/2 flex items-center justify-center">
@@ -158,7 +198,7 @@ const Projects: React.FC = () => {
         </div>
 
 
-        <div className="flex flex-wrap justify-center bg-gray-100 w-200 h-100 mx-auto p-4 mb-30 shadow-md hover:scale-105 transition-transform duration-300">
+        <div className="flex flex-wrap justify-center bg-gray-100 w-200 h-100 mx-auto p-4 mb-30 shadow-md ">
 
         <div className="flex w-full max-w-4xl bg-white rounded-xl  overflow-hidden">
 
@@ -173,6 +213,13 @@ const Projects: React.FC = () => {
               <img src={youtube} /> 
               Demo
             </button> */}
+             <div className='flex mt-5 gap-5 '>
+            <button className='bg-gray-700 text-white px-4 py-2 rounded-4xl flex hover:scale-105 duration-150 cursor-pointer gap-1' 
+            onClick={() => window.open('https://github.com/dinukaviraji/Portfolio-', '_blank')}>
+              <img src={github}/>
+              GitHub
+            </button>
+            </div>
           </div>
 
           <div className="w-full  md:w-1/2 flex items-center justify-center">
